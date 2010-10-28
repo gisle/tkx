8800fb31e2e04039ee5ad0095977ae2f8f8f04b7
Jeff Hobbs <jeffh@activestate.com>
2010-10-28 22:15:52 +0200

    Jeff's example program

---
+++ table.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Tkx;
+
+Tkx::package_require("Tktable");
+
+my $mw = Tkx::widget->new(".");
+my %hash;
+tie %hash, 'Tcl::Var', Tkx::i::interp(), "myarray";
+%hash = ( # data to display
+  '1,1' => 'Goodby',
+  '2,2' => 'cruel',
+  '3,3' => 'world',
+);
+my $t = $mw->new_table(
+    -rows => 5,
+    -cols => 3,
+    -cache => 1,
+    -variable => "myarray",
+);
+$t->g_pack(-fill => 'both', -expand => 1);
+Tkx::MainLoop();
